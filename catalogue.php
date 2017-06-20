5aa5643bbe11df273a685eb3be7ed319cd038ecf
ClemenceLaffont <laffontclemence@gmail.com>
2017-06-20 12:03:58 +0200

    MAJ serveur

--- catalogue.php
+++ catalogue.php
@@ -1,8 +1,9 @@
 <?php 
+$page = "catalogue";
 include_once("header.html");
 ?>
 
-<main>
+<main id="catalogue">
 <?php
     $projets = scandir("..");
     if ($projets === false) {
@@ -10,25 +11,25 @@ include_once("header.html");
         echo 'Oops, une erreur est survenu !';
         exit(1);
     }
-    $projets = array_diff($projets, ['.', '..']);
+    $projets = array_diff($projets, ['.', '..', 'RandomExo', 'portfolio', 'remakewebsite1']);
     foreach ($projets as $projet) {
         if (is_dir("../".$projet)) {
-            echo "<article>";
+            echo "<a class='projets' href='../".$projet."'>";
             if (is_file("../".$projet."/miniature.png")) {
                 echo '<img src="../'.$projet.'/miniature.png"/>';
             }
             if (is_file("../".$projet."/description.json")) {
                 $json = file_get_contents("../".$projet."/description.json");
                 $info = json_decode($json);
-                echo "<a href='../".$projet."'>";
+                echo "<section>";
                 echo "<h3>".$projet."</h3>";
-                echo "<p>Date de création : ".$info->date."</p>";
-                echo "<p>Langages : ".$info->langue."</p>";
-                echo "<p>Consept vue ou appris : ".$info->concept."</p>";
-                echo "<p>Description : ".$info->commentaire."</p>";
-                echo "</a>";
+                echo "<p><strong>Date de création :</strong> ".$info->date."</p>";
+                echo "<p><strong>Langages :</strong> ".$info->langue."</p>";
+                echo "<p><strong>Consept vue ou appris :</strong> ".$info->concept."</p>";
+                echo "<p><strong>Description :</strong> ".$info->commentaire."</p>";
+                echo "</section>";
             }
-            echo "</article>";
+            echo "</a>";
         }
     }
 ?>
